4343ea940cf01704ec2403bac9e9d7e17f1905cc
Ibrahim Heritch <her.ibrahim@hotmail.com>
2021-04-18 12:47:24 -0400

    Added logic to fizzbuzz

--- routes/fizzbuzz.js
+++ routes/fizzbuzz.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET fizzbuzz. */
 router.get('/:N', function(req, res, next) {
-    res.send("Your number is: "+req.params.N)
+    var N = req.params.N
 
+    if( N > 0 && N < 100000 ){
+      var answer = ''
+      if(N%3==0){
+        answer+="Fizz"
+      }
+      if(N%5==0){
+        answer+="Buzz"
+      }
+      if(answer == ''){
+        res.send(N)
+      }else{
+        res.send(answer)
+      }
+    }else{
+      res.status(400).send("Bad request try again");
+    }
 
 });
 
