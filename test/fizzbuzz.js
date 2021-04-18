9b236b4bab220554b31cb464a53cf97877881a3a
Ibrahim Heritch <her.ibrahim@hotmail.com>
2021-04-18 12:47:48 -0400

    Fixed bug in fizzbuzz test

--- test/fizzbuzz.js
+++ test/fizzbuzz.js
@@ -11,7 +11,7 @@ describe('Take Home Assignment', function() {
   it("returns status 200", function() {
       request(url+"1", function(error, response, body) {
         expect(response.statusCode).to.equal(200)
-        done()
+
       })
 
     })
@@ -19,15 +19,15 @@ describe('Take Home Assignment', function() {
    it("returns the number in case not divisible by 3 or 5",  function() {
        request(url+"1", function(error, response, body) {
          expect(response.body).to.equal('1')
-         done()
+
        })
        request(url+"4", function(error, response, body) {
          expect(response.body).to.equal('4')
-         done()
+
        })
        request(url+"44444", function(error, response, body) {
          expect(response.body).to.equal('44444')
-         done()
+
        })
 
      })
@@ -35,15 +35,15 @@ describe('Take Home Assignment', function() {
      it("returns the Fizz in case divisible by 3",  function() {
          request(url+"3", function(error, response, body) {
            expect(body).to.equal("Fizz")
-           done()
+
          })
          request(url+"9", function(error, response, body) {
            expect(body).to.equal("Fizz")
-           done()
+
          })
          request(url+"33333", function(error, response, body) {
            expect(response.body).to.equal("Fizz")
-           done()
+
          })
 
        })
@@ -51,15 +51,15 @@ describe('Take Home Assignment', function() {
        it("returns the Buzz in case divisible by 5",  function() {
            request(url+"5", function(error, response, body) {
              expect(body).to.equal("Buzz")
-             done()
+
            })
            request(url+"10", function(error, response, body) {
              expect(body).to.equal("Buzz")
-             done()
+
            })
            request(url+"55555", function(error, response, body) {
              expect(response.body).to.equal("Buzz")
-             done()
+
            })
 
          })
@@ -67,15 +67,15 @@ describe('Take Home Assignment', function() {
          it("returns the FizzBuzz in case divisible by 3 and 5",  function() {
              request(url+"15", function(error, response, body) {
                expect(body).to.equal("FizzBuzz")
-               done()
+
              })
              request(url+"30", function(error, response, body) {
                expect(body).to.equal("FizzBuzz")
-               done()
+
              })
              request(url+"600", function(error, response, body) {
                expect(response.body).to.equal("FizzBuzz")
-               done()
+
              })
 
            })
@@ -83,17 +83,17 @@ describe('Take Home Assignment', function() {
            it("returns the status 400 in case of illegal input",  function() {
              request(url+"123456", function(error, response, body) {
                expect(response.statusCode).to.equal(400)
-               done()
+
              })
 
              request(url+"hello", function(error, response, body) {
                expect(response.statusCode).to.equal(400)
-               done()
+
              })
 
              request(url+"0", function(error, response, body) {
                expect(response.statusCode).to.equal(400)
-               done()
+  
              })
 
              })
